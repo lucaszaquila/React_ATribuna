c64dec1d1e553a343c4a4c3c2412a5dc19b696a8
Lucas Áquila <lucaszaquila@gmail.com>
2022-03-16 13:53:44 -0300

    Melhora na logica do FiveBlock

--- src/components/layout/sections/FiveBlocks.js
+++ src/components/layout/sections/FiveBlocks.js
@@ -1,48 +1,40 @@
+import { useState, useEffect } from 'react'
+
 import ColumnBlock from '../blocks/ColumnBlock'
 import styles from './FiveBlocks.module.css'
 
 const FiveBlocks = ({articles=[], invert}) => {
+    const [inverted, setInverted] = useState("highlight")
     const highlightArticle = articles.slice(0,1)
     const sideArticles = articles.slice(1,5)
 
+    useEffect(() => {
+        if(invert){
+            setInverted("highlight_inverted")
+         }
+    },[])
 
     return(
         <section className={styles.five_blocks}>
-            {sideArticles?.map((article, index)=>
+            {sideArticles?.map((article)=>
                 <ColumnBlock
                     img={article.image}
                     titulo={article.title}
                     url={article.url}
                     customClass="title_overlay"
-                    key={index}
+                    key={article.title}
                 />
             )}
-            { !invert ? 
-                (<>
-                    {highlightArticle?.map((article, index) =>
-                        <ColumnBlock
-                            img={article.image}
-                            titulo={article.title}
-                            url={article.url}
-                            customClass="title_overlay"
-                            type="highlight"
-                            key={index+100}
-                        />
-                    )}
-                </>) : (<>
-                    {highlightArticle?.map((article, index) =>
-                        <ColumnBlock
-                            img={article.image}
-                            titulo={article.title}
-                            url={article.url}
-                            customClass="title_overlay"
-                            type="highlight_inverted"
-                            key={index+100}
-                        />
-                    )}
-                </>)
-            }
-            
+            {highlightArticle?.map((article) =>
+                <ColumnBlock
+                    img={article.image}
+                    titulo={article.title}
+                    url={article.url}
+                    customClass="title_overlay"
+                    type={inverted}
+                    key={article.title}
+                />
+            )}
         </section>
     )
 }
